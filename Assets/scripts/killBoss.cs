a9cb497adb5cf7ba3ac349a329b67c2cb8ea402d
hugo fagerlind <hugo.fagerlind@elev.ga.lbs.se>
2018-12-04 10:38:07 +0100

    RaidbossUpdates

--- Assets/scripts/killBoss.cs
+++ Assets/scripts/killBoss.cs
@@ -6,17 +6,22 @@ public class killBoss : MonoBehaviour
 {
     public float bossHP = 6;
 
+    public GameObject spawnObject;
+    public Transform[] spawnPoints;
+
     //När vi kommer i kontakt med något så händer något
     private void OnTriggerEnter2D(Collider2D collision)
     {
         //Om något med taggen player nuddar detta objekt så händer något.
         if (collision.tag == "Player")
         {
-            bossHP = bossHP - 1;
+            bossHP = bossHP - 1;  
+            print("hit");
         }
         if (bossHP == 0)
         {
             Destroy(gameObject);
+            Instantiate(spawnObject, spawnPoints[Random.Range(0, spawnPoints.Length)].position, Quaternion.identity);
         }
     }
 }
